93f255f59fa49ce947f7b2035faa1828ab8ca71e
Lyusko <lyudmilborisov@proton.me>
2024-10-10 16:48:03 +0300

    website push

---
+++ scripts.js
@@ -0,0 +1,17 @@
+
+const backToTopBtn = document.querySelector(".backToTop");
+
+
+window.addEventListener("scroll", () => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+});
+
+
+backToTopBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
